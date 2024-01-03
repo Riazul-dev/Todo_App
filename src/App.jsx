75cfc81b9fef4c02f48544741786ea1a750791d5
Riazul-dev <riazul4u@gmail.com>
2024-01-03 13:14:25 +0600

    add input filed

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,17 @@ function App() {
 
   return (
     <div className='min-h-screen bg-gray-600 text-white'>
-      <h1>This is a Todo App</h1>
+      <h1 className='text-3xl text-center py-8'>This is a Todo App</h1>
+      <div className='flex rounded overflow-hidden mx-8'>
+        <input
+          type="text"
+          className='w-full p-2 outline-none text-orange-600 tracking-wider' 
+        />
+        <button
+          type='button'
+          className='bg-green-500 shrink-0 p-2 tracking-wide'
+        >Add Todo</button>
+      </div>
     </div>
   )
 }
