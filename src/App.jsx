76f9603466a8ee0bdddc339338f74126ecf4df47
Riazul-dev <riazul4u@gmail.com>
2024-01-03 12:59:56 +0600

    Add new

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+
+function App() {
+
+  return (
+    <div className='min-h-screen bg-gray-600 text-white'>
+      <h1>This is a Todo App</h1>
+    </div>
+  )
+}
+
+export default App
